fad2b83b4d8e3bb09578b231d6d5f5d1a0109dc9
Aleksandr <aleksandr.molchagin@gmail.com>
2021-03-01 09:46:10 +0300

    Switch to Vue.js

---
+++ react_aleksandrmolchagin.com/src/components/loadscreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, Image } from 'react-native'
+import { Card, ListItem, Button, Icon } from 'react-native-elements'
+
+class Loadscreen extends React.Component {
+    
+  render() {
+    const { classes } = this.props;
+    return (
+        <View>Loadscreen Object</View>
+    );
+  }
+}
+
+export default Loadscreen;
--- react_aleksandrmolchagin.com/src/pages/home.js
+++ react_aleksandrmolchagin.com/src/pages/home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Draggable from 'react-draggable';
 import Window from '../components/window';
+import Loadscreen from '../components/loadscreen';
 import { View, Text, Image } from 'react-native'
 import { Card, ListItem, Button, Icon } from 'react-native-elements'
 
@@ -13,6 +14,7 @@ class home extends Component {
     render(){
         return(
         <View>
+            <Loadscreen></Loadscreen>
             <Window></Window>
             <Window></Window>
         </View>
