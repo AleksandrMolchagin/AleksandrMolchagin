6c1c11dd53884a005e161b3d0dd72b58d3037961
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2021-11-26 20:24:53 -0500

    Add Basic Structure

---
+++ react_v2/src/desktop/Desktop.tsx
@@ -0,0 +1,18 @@
+import './Desktop.css'
+
+export function Desktop(): JSX.Element{
+
+    return(
+        <div className="Desktop-container">
+            <div className="Header-container">
+            yes
+            </div>
+            <div className="Applications-container">
+
+            </div>
+            <div className="Dock-container">
+
+            </div>
+        </div>
+    )
+}
