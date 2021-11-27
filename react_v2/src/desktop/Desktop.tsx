5886643dc2f289074a1d719582ec560cb1317ac5
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2021-11-26 20:47:07 -0500

    Add timer

--- react_v2/src/desktop/Desktop.tsx
+++ react_v2/src/desktop/Desktop.tsx
@@ -1,11 +1,14 @@
 import './Desktop.css'
+import Moment from 'react-moment';
 
 export function Desktop(): JSX.Element{
+
+    const dateToFormat = '1976-04-19T12:59';
 
     return(
         <div className="Desktop-container">
             <div className="Header-container">
-            yes
+                <div className="Header-timer"><Moment interval={1000} format="dd MM/d h:mm:ss"/></div>
             </div>
             <div className="Applications-container">
 
