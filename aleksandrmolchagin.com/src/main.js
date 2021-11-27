4ff474bd0ba28c8eb8349ab34ac01997a3448fa7
AleksandrMolchagin <contact@aleksandrmolchagin.com>
2021-11-26 19:50:18 -0500

    Delete old

--- aleksandrmolchagin.com/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
-import installElementPlus from './plugins/element'
-
-const app = createApp(App)
-installElementPlus(app)
-app.use(store).mount('#app')
